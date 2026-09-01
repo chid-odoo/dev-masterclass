2f84ab1952915e1bbab4498f2f5e0a75277cd1e2
Danny Chicchon (chid) <chid@odoo.com>
2026-09-01 16:31:26 -0700

    test

--- kawiil_financing/__manifest__.py
+++ kawiil_financing/__manifest__.py
@@ -6,7 +6,7 @@
     "website": "https://github.com/odoo-trainings/development-masterclass",
     "version": "1.0.0",
     "author": "ODOP Trainee",
-    "depends": ["contacts", "product"],
+    "depends": ["mail", "contacts", "product"],
     "license": "OPL-1",
     "data": [
         "security/kawiil_financing_groups.xml",
